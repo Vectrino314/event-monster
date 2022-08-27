879c3e93431f494381be04ccd50ebabe90e2c16d
Spiegelberg <jesperspiegelberg@gmail.com>
2022-08-27 14:00:03 +0200

    ux for Sign up and Sign in

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -15,9 +15,18 @@
   ```
 -->
 <script setup lang="ts">
+let signingUp = ref(false)
 const { getProviderAuthenticationUrl } = useStrapiAuth()
 const onDiscordClick = () => {
   window.location = getProviderAuthenticationUrl('discord')
+}
+const sendMessage = () => {
+  signingUp = true
+  // console.log('start', signingUp)
+  setTimeout(() => {
+    signingUp = ref(false)
+    // console.log('end', signingUp)
+  }, 10000)
 }
 </script>
 
@@ -30,82 +39,114 @@ const onDiscordClick = () => {
     <body class="h-full">
     ```
   -->
-  <div class="flex flex-col justify-center min-h-full py-12 sm:px-6 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="w-auto h-12 mx-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow">
-      <h2 class="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">
-        Sign in to your account
-      </h2>
-      <p class="mt-2 text-sm text-center text-gray-600">
-        Or
-        {{ ' ' }}
-        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
-      </p>
-    </div>
+  <div class="flex flex-col">
+    <div class="flex flex-col-12">
+      <div class="flex flex-col justify-center min-h-full py-12 sm:px-6 lg:px-8" style="width:50%;">
+        <div class="flex flex-col justify-start">
+          <img class="w-auto h-12 mx-auto" src="/EventMonster Logo Web.png" alt="Workflow">
+        </div>
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 class="mt-6 mb-6 text-3xl font-bold tracking-tight text-center text-gray-900">
+            Create your account ✨
+          </h2>
+          <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+            <form class="space-y-6" action="#" method="POST" @submit.prevent="sendMessage()">
+              <div>
+                <label for="email" class="block text-sm font-medium text-gray-700"> Email address <span class="text-red-900">*</span></label>
+                <div class="mt-1">
+                  <input id="email" name="email" type="email" autocomplete="email" required="true" class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </div>
+              </div>
 
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
-        <form class="space-y-6" action="#" method="POST">
-          <div>
-            <label for="email" class="block text-sm font-medium text-gray-700"> Email address </label>
-            <div class="mt-1">
-              <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-            </div>
-          </div>
+              <div>
+                <label for="fullname" class="block text-sm font-medium text-gray-700"> Full name <span class="text-red-900">*</span></label>
+                <div class="mt-1">
+                  <input id="fullname" name="fullname" type="fullname" autocomplete="fullname" required="true" class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </div>
+              </div>
 
-          <div>
-            <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
-            <div class="mt-1">
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-            </div>
-          </div>
+              <div>
+                <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
+                <div class="mt-1">
+                  <input id="password" name="password" type="password" autocomplete="current-password" required="true" class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </div>
+              </div>
 
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <input id="remember-me" name="remember-me" type="checkbox" class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500">
-              <label for="remember-me" class="block ml-2 text-sm text-gray-900"> Remember me </label>
-            </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                  <input id="remember-me" name="remember-me" type="checkbox" class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500">
+                  <label for="remember-me" class="block ml-2 text-sm text-gray-900"> Email me about EventMonster news </label>
+                </div>
 
-            <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
-            </div>
-          </div>
+                <!-- <div class="text-sm">
+                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+                </div> -->
+                <div>
+                  {{ signingUp }}
+                  <button
+                    type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    @click="sendMessage"
+                  >
+                    <template v-if="signingUp">
+                      <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+                      </svg>
+                      Processing...
+                    </template>
+                    <template v-else>
+                      Sign up
+                    </template>
+                  </button>
+                </div>
+              </div>
+            </form>
 
-          <div>
-            <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Sign in
-            </button>
-          </div>
-        </form>
+            <div class="mt-6">
+              <div class="relative">
+                <div class="absolute inset-0 flex items-center">
+                  <div class="w-full border-t border-gray-300" />
+                </div>
+                <div class="relative flex justify-center text-sm">
+                  <span class="px-2 text-gray-500 bg-white">or create account with</span>
+                </div>
+              </div>
 
-        <div class="mt-6">
-          <div class="relative">
-            <div class="absolute inset-0 flex items-center">
-              <div class="w-full border-t border-gray-300" />
-            </div>
-            <div class="relative flex justify-center text-sm">
-              <span class="px-2 text-gray-500 bg-white"> Or continue with </span>
-            </div>
-          </div>
-
-          <div class="grid grid-cols-3 gap-3 mt-6">
-            <div>
-              <a class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50" @click="onDiscordClick()">
-                <span class="sr-only">Sign in with Discord</span>
-                Discord
-              </a>
-            </div>
-
-            <div>
-              <a href="#" class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
-                <span class="sr-only">Sign in with Twitter</span>
-                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
+              <div class="grid grid-row-3 gap-3 mt-6">
+                <div>
+                  <a href="#" class="inline-flex justify-start w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
+                    <span class="sr-only">Sign up with Google</span>
+                    <img src="/assets/svg/google.svg" style="width: 20px;height:20px">
+                    <div class="inline-flex justify-center w-full">
+                      Google
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="#" class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
+                    <span class="sr-only">Sign up with Twitter</span>
+                    <img src="/assets/svg/twitter.svg" style="width: 20px;height:20px">
+                    <div class="inline-flex justify-center w-full">
+                      Twitter
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50" @click="onDiscordClick()">
+                    <span class="sr-only">Sign up with Discord</span>
+                    <img src="/assets/svg/discord.svg" style="width: 20px;height:20px">
+                    <div class="inline-flex justify-center w-full">
+                      Discord
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="flex flex-col justify-center min-h-full py-12 sm:px-6 lg:px-8 bg-local bg-no-repeat object-cover" style="width:50%;background-image: url(/assets/images/man-couch-laptop-meeting.jpg)">
+        &nbsp;
       </div>
     </div>
   </div>
