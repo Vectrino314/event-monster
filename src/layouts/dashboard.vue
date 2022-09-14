847c6cc465ee3d200692de65ef8c4da11d5011c6
Vectrino314 <vectrino314@gmail.com>
2022-09-14 16:35:02 -0500

    Removed default dashboard items

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -255,16 +255,16 @@ const sidebarOpen = ref(false)
 
       <main>
         <div class="py-6">
-          <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">
-              Dashboard
-            </h1>
-          </div>
+          <!--          <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8"> -->
+          <!--            <h1 class="text-2xl font-semibold text-gray-900"> -->
+          <!--              Dashboard -->
+          <!--            </h1> -->
+          <!--          </div> -->
           <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
             <div class="py-4">
               <slot />
-              <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+<!--              <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />-->
             </div>
             <!-- /End replace -->
           </div>
